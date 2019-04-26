a6e3add93ec9b26205eadb4150a234a015dd9238
Brittney VandenBos <bvandenbos@decwise.local>
2019-04-26 08:57:26 -0600

    April 26

--- db/get_survey_by_company_id.sql
+++ db/get_survey_by_company_id.sql
@@ -2,4 +2,4 @@ SELECT survey.id, survey.index, q_id, q_text, q_category, q_dimension, q_dimensi
 FROM survey
 JOIN dimension ON dimension.id = survey.q_dimension_id
 WHERE survey.company_id = $1
-ORDER BY survey.index asc;
+ORDER BY dimension.index asc, survey.index asc;

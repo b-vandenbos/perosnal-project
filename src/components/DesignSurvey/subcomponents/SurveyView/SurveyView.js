5dad537e6b36d871716328681a2629cf2606e8c2
Brittney VandenBos <bvandenbos@decwise.local>
2019-04-30 11:20:37 -0600

    April 30

--- src/components/DesignSurvey/subcomponents/SurveyView/SurveyView.js
+++ src/components/DesignSurvey/subcomponents/SurveyView/SurveyView.js
@@ -18,6 +18,7 @@ class SurveyView extends Component {
         
         this.socket = io('/', {transports: ['websocket']});
         this.socket.on('RECEIVE_DIMENSIONS', function(data) {
+            console.log(data);
             updateDimensions(data);
         });
         this.socket.on('RECEIVE_DIM_SURVEY_SUGGESTED', function(data) {
@@ -26,7 +27,6 @@ class SurveyView extends Component {
 
         const updateDimensions = data => {
             this.setState({dimensions: data});
-            console.log(this.state.dimensions);
         }
 
 
